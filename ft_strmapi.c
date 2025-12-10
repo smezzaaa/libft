89716d73d18e6553508cd4eb6c49af94c93c9960
sofia <sofia.mezar06@gmail.com>
2025-12-10 21:53:44 +0100

    ft_strmapi added

---
+++ ft_strmapi.c
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: smeza-ro <smeza-ro@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/10 21:04:50 by smeza-ro          #+#    #+#             */
+/*   Updated: 2025/12/10 21:48:46 by smeza-ro         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	f(unsigned int i, char c)
+{
+	char	x;
+	
+	x = c + 32;
+	return (x);
+}
+
+char *ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	unsigned int	i;
+	unsigned int 	len;
+	char			*news;
+
+	len = 0;
+	i = 0;
+	if (!s)
+		return(NULL);
+	while (s[len])
+		len++;
+	news = (char *)malloc(sizeof(char) * (len + 1));
+	if (!news)
+		return(NULL);
+	while (i < len)
+	{
+		news[i] = f(i, s[i]);
+		i++;
+	}
+	news [i] = 0;
+	return (news);
+}
+int main()
+{
+	char str[] = "blob";
+	char	*s = ft_strmapi(str, f);
+	printf ("%s\n", s);
+	free(s);
+}
